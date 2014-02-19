706408eb73f08081400c4789a1f6d387bd8f26ce
Tino <Tino@172.16.48.146>
2014-02-19 15:39:23 +0100

    Klasse Ausleihe und Notebook hinzugefügt

---
+++ src/java/BL/Ausleihe.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Tino
+ */
+public class Ausleihe {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    
+     @Temporal (javax.persistence.TemporalType.DATE)
+    private Date auftragsdatum;
+    private Student antragssteller;
+    private Dozent mitarbeiter;
+    private int dauer;
+    
+    @Temporal (javax.persistence.TemporalType.DATE)
+    private Date von;
+    
+    @Temporal (javax.persistence.TemporalType.DATE)
+    private Date bis;
+    
+    private String bermerkung;
+    private String status;
+    private Notebook leihNotebook;
+
+    public Ausleihe(Date auftragsdatum, Student antragssteller, Dozent mitarbeiter, int dauer, Date von, Date bis, String bermerkung, String status, Notebook leihNotebook) {
+        this.auftragsdatum = auftragsdatum;
+        this.antragssteller = antragssteller;
+        this.mitarbeiter = mitarbeiter;
+        this.dauer = dauer;
+        this.von = von;
+        this.bis = bis;
+        this.bermerkung = bermerkung;
+        this.status = status;
+        this.leihNotebook = leihNotebook;
+    }
+
+    public Ausleihe() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getAuftragsdatum() {
+        return auftragsdatum;
+    }
+
+    public void setAuftragsdatum(Date auftragsdatum) {
+        this.auftragsdatum = auftragsdatum;
+    }
+
+    public Student getAntragssteller() {
+        return antragssteller;
+    }
+
+    public void setAntragssteller(Student antragssteller) {
+        this.antragssteller = antragssteller;
+    }
+
+    public Dozent getMitarbeiter() {
+        return mitarbeiter;
+    }
+
+    public void setMitarbeiter(Dozent mitarbeiter) {
+        this.mitarbeiter = mitarbeiter;
+    }
+
+    public int getDauer() {
+        return dauer;
+    }
+
+    public void setDauer(int dauer) {
+        this.dauer = dauer;
+    }
+
+    public Date getVon() {
+        return von;
+    }
+
+    public void setVon(Date von) {
+        this.von = von;
+    }
+
+    public Date getBis() {
+        return bis;
+    }
+
+    public void setBis(Date bis) {
+        this.bis = bis;
+    }
+
+    public String getBermerkung() {
+        return bermerkung;
+    }
+
+    public void setBermerkung(String bermerkung) {
+        this.bermerkung = bermerkung;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Notebook getLeihNotebook() {
+        return leihNotebook;
+    }
+
+    public void setLeihNotebook(Notebook leihNotebook) {
+        this.leihNotebook = leihNotebook;
+    }
+    
+    
+    
+}
---
+++ src/java/BL/Notebook.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Tino
+ */
+public class Notebook {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String seriennummer;
+    private int klasse;
+    private int leihdauer;
+    private boolean verliehen;
+
+    public Notebook(String name, String seriennummer, int klasse, int leihdauer, boolean verliehen) {
+        this.name = name;
+        this.seriennummer = seriennummer;
+        this.klasse = klasse;
+        this.leihdauer = leihdauer;
+        this.verliehen = verliehen;
+    }
+
+    public Notebook() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeriennummer() {
+        return seriennummer;
+    }
+
+    public void setSeriennummer(String seriennummer) {
+        this.seriennummer = seriennummer;
+    }
+
+    public int getKlasse() {
+        return klasse;
+    }
+
+    public void setKlasse(int klasse) {
+        this.klasse = klasse;
+    }
+
+    public int getLeihdauer() {
+        return leihdauer;
+    }
+
+    public void setLeihdauer(int leihdauer) {
+        this.leihdauer = leihdauer;
+    }
+
+    public boolean isVerliehen() {
+        return verliehen;
+    }
+
+    public void setVerliehen(boolean verliehen) {
+        this.verliehen = verliehen;
+    }
+    
+    
+    
+}
